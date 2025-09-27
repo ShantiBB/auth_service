e4eb4a72c466c0835c44ef5137c6950677401693
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-27 23:38:29 +0300

    Add user entities

---
+++ internal/entity/user.go
@@ -0,0 +1,35 @@
+package entity
+
+type UserCreate struct {
+	Username    string `db:"username"`
+	FirstName   string `db:"first_name"`
+	LastName    string `db:"last_name"`
+	Email       string `db:"email"`
+	Description string `db:"description"`
+	Password    string `db:"password"`
+}
+
+type User struct {
+	ID          int64  `db:"id"`
+	Username    string `db:"username"`
+	FirstName   string `db:"first_name"`
+	LastName    string `db:"last_name"`
+	Email       string `db:"email"`
+	Description string `db:"description"`
+}
+
+type UserUpdatePassword struct {
+	ID          int64
+	Password    string
+	NewPassword string
+}
+
+func (u UserCreate) ToUserRead() *User {
+	return &User{
+		Username:    u.Username,
+		FirstName:   u.FirstName,
+		LastName:    u.LastName,
+		Email:       u.Email,
+		Description: u.Description,
+	}
+}
