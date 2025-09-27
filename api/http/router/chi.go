7a72c995de6d3be30ff75923d0e301cfd3bc6a34
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-28 01:33:09 +0300

    Add user router

---
+++ api/http/router/chi.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"auth_service/api/http/handler"
+)
+
+func New(r chi.Router, h *handler.Handler) {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", h.Create)
+	})
+}
