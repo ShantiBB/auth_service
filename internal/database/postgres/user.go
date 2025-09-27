e27dd2dedaded675db4b0226e55ea6de36cd2e2d
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-28 01:32:07 +0300

    Add context

--- internal/database/postgres/user.go
+++ internal/database/postgres/user.go
@@ -1,22 +1,23 @@
 package postgres
 
 import (
+	"context"
 	"fmt"
 
 	"auth_service/internal/entity"
 )
 
 type UserRepository interface {
-	Create(user entity.UserCreate) (*entity.User, error)
-	Get(id int64) (*entity.User, error)
-	GetAll() ([]entity.User, error)
-	Update(user entity.User) (*entity.User, error)
-	Delete(id int64) error
+	Create(ctx context.Context, user entity.UserCreate) (*entity.User, error)
+	Get(ctx context.Context, id int64) (*entity.User, error)
+	GetAll(ctx context.Context) ([]entity.User, error)
+	Update(ctx context.Context, user entity.User) error
+	Delete(ctx context.Context, id int64) error
 }
 
-func (p *Repository) Create(user entity.UserCreate) (*entity.User, error) {
+func (p *Repository) Create(ctx context.Context, user entity.UserCreate) (*entity.User, error) {
 	newUser := user.ToUserRead()
-	rows, err := p.db.NamedQuery(UserCreate, user)
+	rows, err := p.db.NamedQueryContext(ctx, UserCreate, user)
 	if err != nil {
 		return nil, err
 	}
@@ -31,49 +32,44 @@ func (p *Repository) Create(user entity.UserCreate) (*entity.User, error) {
 	return newUser, nil
 }
 
-func (p *Repository) Get(id int64) (*entity.User, error) {
+func (p *Repository) Get(ctx context.Context, id int64) (*entity.User, error) {
 	var user entity.User
 
-	if err := p.db.Get(&user, UserGet, id); err != nil {
+	if err := p.db.GetContext(ctx, &user, UserGet, id); err != nil {
 		return nil, err
 	}
 
 	return &user, nil
 }
 
-func (p *Repository) GetAll() ([]entity.User, error) {
+func (p *Repository) GetAll(ctx context.Context) ([]entity.User, error) {
 	var users []entity.User
 
-	if err := p.db.Select(&users, UserGetAll); err != nil {
+	if err := p.db.SelectContext(ctx, &users, UserGetAll); err != nil {
 		return nil, err
 	}
 
 	return users, nil
 }
 
-func (p *Repository) Update(user entity.User) (*entity.User, error) {
-	rows, err := p.db.NamedExec(UserUpdate, &user)
+func (p *Repository) Update(ctx context.Context, user entity.User) error {
+	rows, err := p.db.NamedExecContext(ctx, UserUpdate, &user)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	rowsAffected, err := rows.RowsAffected()
 	if err != nil {
-		return nil, err
+		return err
 	} else if rowsAffected == 0 {
-		return nil, fmt.Errorf("user with id %d is not found", user.ID)
+		return fmt.Errorf("user with id %d is not found", user.ID)
 	}
 
-	updateUser, err := p.Get(user.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	return updateUser, nil
+	return nil
 }
 
-func (p *Repository) Delete(id int64) error {
-	rows, err := p.db.Exec(UserDelete, id)
+func (p *Repository) Delete(ctx context.Context, id int64) error {
+	rows, err := p.db.ExecContext(ctx, UserDelete, id)
 	if err != nil {
 		return err
 	}
---
+++ internal/service/user.go
@@ -0,0 +1,64 @@
+package service
+
+import (
+	"context"
+
+	"auth_service/internal/entity"
+)
+
+type UserService interface {
+	Create(ctx context.Context, user entity.UserCreate) (*entity.User, error)
+	Get(ctx context.Context, id int64) (*entity.User, error)
+	GetAll(ctx context.Context) ([]entity.User, error)
+	Update(ctx context.Context, user entity.User) (*entity.User, error)
+	Delete(ctx context.Context, id int64) error
+}
+
+func (s Service) Create(ctx context.Context, user entity.UserCreate) (*entity.User, error) {
+	newUser, err := s.UserRepo.Create(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return newUser, nil
+}
+
+func (s Service) Get(ctx context.Context, id int64) (*entity.User, error) {
+	user, err := s.UserRepo.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s Service) GetAll(ctx context.Context) ([]entity.User, error) {
+	users, err := s.UserRepo.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s Service) Update(ctx context.Context, user entity.User) (*entity.User, error) {
+	if err := s.UserRepo.Update(ctx, user); err != nil {
+		return nil, err
+	}
+
+	updatedUser, err := s.UserRepo.Get(ctx, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedUser, nil
+}
+
+func (s Service) Delete(ctx context.Context, id int64) error {
+	err := s.UserRepo.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
