8f2ec30319082890a9c8f8b62873fe0cc8c0c6b0
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-28 01:33:28 +0300

    Add user request schema

---
+++ api/http/schemas/request.go
@@ -0,0 +1,18 @@
+package schemas
+
+type UserCreateRequest struct {
+	Username    string `json:"username" validate:"required"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email" validate:"required,email"`
+	Description string `json:"description"`
+	Password    string `json:"password" validate:"required,min=5"`
+}
+
+type UserUpdateRequest struct {
+	Username    string `json:"username"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email" validate:"email"`
+	Description string `json:"description"`
+}
