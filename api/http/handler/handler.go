d63923bf0abb97027f48d010b95bba785742b647
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-28 18:47:18 +0300

    Add transform objs

--- api/http/handler/handler.go
+++ api/http/handler/handler.go
@@ -1,6 +1,10 @@
 package handler
 
-import "auth_service/internal/service"
+import (
+	"auth_service/api/http/schemas"
+	"auth_service/internal/entity"
+	"auth_service/internal/service"
+)
 
 type Handler struct {
 	UserService service.UserService
@@ -9,5 +13,27 @@ type Handler struct {
 func New(userService service.UserService) *Handler {
 	return &Handler{
 		UserService: userService,
+	}
+}
+
+func (h *Handler) UserCreateRequestToEntity(req *schemas.UserCreateRequest, hash string) *entity.UserCreate {
+	return &entity.UserCreate{
+		Username:    req.Username,
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		Email:       req.Email,
+		Description: req.Description,
+		Password:    hash,
+	}
+}
+
+func (h *Handler) UserEntityToResponse(user *entity.User) *schemas.UserResponse {
+	return &schemas.UserResponse{
+		ID:          user.ID,
+		Username:    user.Username,
+		FirstName:   user.FirstName,
+		LastName:    user.LastName,
+		Email:       user.Email,
+		Description: user.Description,
 	}
 }
