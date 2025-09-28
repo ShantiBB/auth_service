54f44228828cdf4e0f0d3e4222c9464174650a6f
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-09-28 18:48:52 +0300

    Add handlers

--- internal/service/user.go
+++ internal/service/user.go
@@ -14,7 +14,7 @@ type UserService interface {
 	Delete(ctx context.Context, id int64) error
 }
 
-func (s Service) Create(ctx context.Context, user entity.UserCreate) (*entity.User, error) {
+func (s *Service) Create(ctx context.Context, user entity.UserCreate) (*entity.User, error) {
 	newUser, err := s.UserRepo.Create(ctx, user)
 	if err != nil {
 		return nil, err
@@ -23,7 +23,7 @@ func (s Service) Create(ctx context.Context, user entity.UserCreate) (*entity.Us
 	return newUser, nil
 }
 
-func (s Service) Get(ctx context.Context, id int64) (*entity.User, error) {
+func (s *Service) Get(ctx context.Context, id int64) (*entity.User, error) {
 	user, err := s.UserRepo.Get(ctx, id)
 	if err != nil {
 		return nil, err
@@ -32,7 +32,7 @@ func (s Service) Get(ctx context.Context, id int64) (*entity.User, error) {
 	return user, nil
 }
 
-func (s Service) GetAll(ctx context.Context) ([]entity.User, error) {
+func (s *Service) GetAll(ctx context.Context) ([]entity.User, error) {
 	users, err := s.UserRepo.GetAll(ctx)
 	if err != nil {
 		return nil, err
@@ -41,7 +41,7 @@ func (s Service) GetAll(ctx context.Context) ([]entity.User, error) {
 	return users, nil
 }
 
-func (s Service) Update(ctx context.Context, user entity.User) (*entity.User, error) {
+func (s *Service) Update(ctx context.Context, user entity.User) (*entity.User, error) {
 	if err := s.UserRepo.Update(ctx, user); err != nil {
 		return nil, err
 	}
@@ -54,7 +54,7 @@ func (s Service) Update(ctx context.Context, user entity.User) (*entity.User, er
 	return updatedUser, nil
 }
 
-func (s Service) Delete(ctx context.Context, id int64) error {
+func (s *Service) Delete(ctx context.Context, id int64) error {
 	err := s.UserRepo.Delete(ctx, id)
 	if err != nil {
 		return err
