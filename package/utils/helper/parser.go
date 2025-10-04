c28d6f4ce553ae8e7c9c0cedbc537eabc090e91b
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-10-05 00:37:28 +0300

    move helper to pkg dir

--- package/utils/helper/parser.go
+++ package/utils/helper/parser.go
@@ -1,4 +1,4 @@
-package handler
+package helper
 
 import (
 	"net/http"
@@ -9,15 +9,15 @@ import (
 	"auth_service/internal/http/lib/validation"
 )
 
-func (h *Handler) ParseJSON(w http.ResponseWriter, r *http.Request, v interface{}) bool {
+func ParseJSON(w http.ResponseWriter, r *http.Request, v interface{}) bool {
 	if err := render.DecodeJSON(r.Body, v); err != nil {
 		errMsg := schemas.NewErrorResponse("Invalid JSON body")
-		h.sendError(w, r, http.StatusBadRequest, errMsg)
+		SendError(w, r, http.StatusBadRequest, errMsg)
 		return false
 	}
 
 	if errResp := validation.CheckErrors(v); errResp != nil {
-		h.sendError(w, r, http.StatusBadRequest, errResp)
+		SendError(w, r, http.StatusBadRequest, errResp)
 		return false
 	}
 
