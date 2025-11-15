20e28cdb3a9f3d6448bbbf057a45377938da134d
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-11-15 23:41:56 +0300

    Add min len for password

--- internal/http/lib/schemas/request/user.go
+++ internal/http/lib/schemas/request/user.go
@@ -3,7 +3,7 @@ package request
 type UserCreate struct {
 	Username string `json:"username" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=5"`
+	Password string `json:"password" validate:"required,min=8"`
 }
 
 type UserUpdate struct {
