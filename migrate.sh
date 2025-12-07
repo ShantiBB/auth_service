9e9c7441a5c586e21c6d10a83e807bab2de60468
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2025-12-07 16:04:21 +0300

    Update migrator script

--- migrate.sh
+++ migrate.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
-USER=$(yq e '.postgres.user' $CONFIG_PATH)
-PASSWORD=$(yq e '.postgres.password' $CONFIG_PATH)
-HOST=$(yq e '.postgres.host' $CONFIG_PATH)
-PORT=$(yq e '.postgres.port' $CONFIG_PATH)
-DB=$(yq e '.postgres.db' $CONFIG_PATH)
-SSLMODE=$(yq e '.postgres.sslmode' $CONFIG_PATH)
+SERVICE=""
+ACTION=""
+STEPS=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --service)
+      SERVICE="$2"
+      shift 2
+      ;;
+    --action)
+      ACTION="$2"
+      shift 2
+      ;;
+    *)
+      if [[ "$1" =~ ^[0-9]+$ ]]; then
+        STEPS="$1"
+      fi
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$SERVICE" ] || [ -z "$ACTION" ]; then
+  echo "Usage: $0 --service <service> --action <up|down|force|version> [steps]"
+  exit 1
+fi
+
+USER=$(yq e '.postgres.user' "$CONFIG_PATH")
+PASSWORD=$(yq e '.postgres.password' "$CONFIG_PATH")
+HOST=$(yq e '.postgres.host' "$CONFIG_PATH")
+PORT=$(yq e '.postgres.port' "$CONFIG_PATH")
+DB=$(yq e '.postgres.db' "$CONFIG_PATH")
+SSLMODE=$(yq e '.postgres.sslmode' "$CONFIG_PATH")
 
 DB_URL="postgres://$USER:$PASSWORD@$HOST:$PORT/$DB?sslmode=$SSLMODE"
+MIGRATION_PATH="./services/$SERVICE/migrations"
 
-migrate -path ./services/auth/migrations -database "$DB_URL" $1 # up / down
+if [ -n "$STEPS" ]; then
+  migrate -path "$MIGRATION_PATH" -database "$DB_URL" "$ACTION" "$STEPS"
+else
+  migrate -path "$MIGRATION_PATH" -database "$DB_URL" "$ACTION"
+fi
