91f8ce431da334f9e2fc33edbb0abf08fe3c4f61
segei.golobokov <segeygolobokov@gmail.com>
2026-01-03 16:40:06 +0300

    Add room queries

---
+++ services/hotel/internal/repository/postgres/query/room.go
@@ -0,0 +1,64 @@
+package query
+
+const (
+	RoomCreateQuery = `
+	INSERT INTO room (hotel_id,
+	                  room_number,
+	                  type,
+	                  price,
+	                  capacity,
+	                  area_sqm,
+	                  floor,
+	                  description,
+	                  amenities,
+	                  images)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+	RETURNING id, created_at, updated_at`
+
+	RoomGetByID = `
+	SELECT id,
+	       hotel_id,
+		   room_number,
+		   type,
+		   price,
+		   capacity,
+		   area_sqm,
+		   floor,
+		   description,
+		   amenities,
+		   images
+	FROM room 
+	WHERE id = $1`
+
+	RoomGetAll = `
+	SELECT id,
+	       hotel_id,
+		   room_number,
+		   type,
+		   price,
+		   capacity,
+		   area_sqm,
+		   floor,
+		   amenities,
+		   images
+	FROM room
+	ORDER BY name
+	LIMIT $1 OFFSET $2;`
+
+	RoomUpdateByID = `
+	UPDATE room 
+	SET price = $1,
+		capacity = $2,
+		area_sqm = $3,
+		floor = $4,
+		description = $5,
+		amenities = $6,
+		images = $7
+	WHERE id = $6;`
+
+	RoomDeleteByID = `
+	DELETE FROM room 
+	WHERE id = $1;`
+
+	RoomGetCountRows = `SELECT COUNT(*) FROM room;`
+)
