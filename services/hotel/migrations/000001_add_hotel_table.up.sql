454726e24da7603d18de5f3357cf9bd03d68100c
segei.golobokov <segeygolobokov@gmail.com>
2026-01-07 12:44:20 +0300

    Add country and city fields. Add title slug

--- services/hotel/migrations/000001_add_hotel_table.up.sql
+++ services/hotel/migrations/000001_add_hotel_table.up.sql
@@ -2,19 +2,23 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 
 CREATE TABLE hotel (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(100) UNIQUE NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    slug VARCHAR(100) NOT NULL CHECK (slug ~ '^[a-z0-9]+(-[a-z0-9]+)*$'),
     owner_id BIGINT NOT NULL,
+    country_code CHAR(2) NOT NULL CHECK (country_code ~ '^[a-z]{2}$'),
+    city_slug VARCHAR(100) NOT NULL CHECK (city_slug ~ '^[a-z0-9]+(-[a-z0-9]+)*$'),
     description TEXT,
     address TEXT NOT NULL,
     location GEOGRAPHY(Point, 4326) NOT NULL,
     rating NUMERIC(3,2) CHECK (rating >= 0 AND rating <= 5),
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE(country_code, city_slug, slug)
 );
 
 CREATE INDEX hotels_location_idx ON hotel USING GIST (location);
 CREATE INDEX hotels_owner_idx ON hotel (owner_id);
-CREATE INDEX hotels_name_idx ON hotel (name);
 
 CREATE OR REPLACE FUNCTION update_updated_at_column()
     RETURNS TRIGGER AS $$
