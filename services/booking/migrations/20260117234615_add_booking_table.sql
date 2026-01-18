b4d1583892aa28304a65d1e5df72c2ca880f3b45
sergei.golobokov <sergei.golobokov@uno-soft.ru>
2026-01-18 20:23:06 +0300

    design

--- services/booking/migrations/20260117234615_add_booking_table.sql
+++ services/booking/migrations/20260117234615_add_booking_table.sql
@@ -7,23 +7,17 @@ CREATE TYPE booking_status AS ENUM ('pending', 'confirmed', 'cancelled');
 
 CREATE TABLE IF NOT EXISTS booking (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     user_id BIGINT NOT NULL,
     hotel_id UUID NOT NULL,
-
     check_in  DATE NOT NULL,
     check_out DATE NOT NULL CHECK (check_out > check_in),
-
     status booking_status NOT NULL DEFAULT 'pending',
-
     guest_name  TEXT NOT NULL,
     guest_email TEXT,
     guest_phone TEXT,
-
     currency CHAR(3) NOT NULL CHECK (currency ~ '^[A-Z]{3}$'),
     expected_total_amount NUMERIC(12,2) NOT NULL CHECK (expected_total_amount >= 0),
     final_total_amount NUMERIC(12,2) NOT NULL CHECK (final_total_amount >= 0),
-
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- services/booking/migrations/20260117234621_add_booking_room_table.sql
+++ services/booking/migrations/20260117234621_add_booking_room_table.sql
@@ -3,14 +3,10 @@
 CREATE TABLE IF NOT EXISTS booking_room (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     booking_id UUID NOT NULL REFERENCES booking(id) ON DELETE CASCADE,
-
     room_id UUID NOT NULL,
-
     adults INT8 NOT NULL DEFAULT 1 CHECK (adults >= 1 AND adults <= 10),
     children INT8 NOT NULL DEFAULT 0 CHECK (children >= 0 AND children <= 10),
-
     price_per_night NUMERIC(12,2) NOT NULL CHECK (price_per_night >= 0),
-
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
